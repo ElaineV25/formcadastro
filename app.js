ca50836c0db73d43eec6cea4b21c5d49cce544aa
elaine <elainevieira@hotmail.com>
2021-08-26 10:55:35 -0300

    mudanca app para dir raiz

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser")
+const candidatos = require("./models/Cadastro")
+const  swaggerUi  =  require ( 'swagger-ui-express' ) ; 
+const  swaggerDocument  =  require ( './swagger.json' ) 
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Rotas
+app.get('/candidatos', function (req, res) {
+    res.render('candidatos');
+});
+
+app.get('/cad-candidatos', function (req, res) {
+    res.render('cad-candidatos');
+});
+
+app.post('/add-candidatos', function (req, res) {
+     candidatos.create({
+         nome: req.body.nome,
+         cargo: req.body.cargo
+     }).then(function(){
+         res.redirect('/candidatos')
+         //res.send("Cadastro com sucesso")
+    }).catch(function(erro){
+        res.send("Não foi cadastrado com sucesso" + erro)
+    }) 
+    //res.send("Nome: " + req.body.nome + "<br>Cargo: " + req.body.cargo)
+})
+
+app.listen(3000);
