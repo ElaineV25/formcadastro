35455733d858bb7dec40c97e8c90bbb128cd0f66
elaine <elainevieira@hotmail.com>
2021-08-26 18:13:23 -0300

    projeto atualização

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser")
-const candidatos = require("./models/Cadastro")
-const  swaggerUi  =  require ( 'swagger-ui-express' ) ; 
-const  swaggerDocument  =  require ( './swagger.json' ) 
+const candidatos = require("./modules/Cadastro")
+//const  swaggerUi  =  require ( 'swagger-ui-express' ) ; 
+//const  swaggerDocument  =  require ( './swagger.json' ) 
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -13,13 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Rotas
-app.get('/candidatos', function (req, res) {
-    res.render('candidatos');
+app.get("/", function (req, res) {
+    res.send("Seja bem vindo");
 });
 
-app.get('/cad-candidatos', function (req, res) {
-    res.render('cad-candidatos');
+app.get('/candidatos', function (req, res) {
+    res.render('./layouts/candidatos');
 });
+
 
 app.post('/add-candidatos', function (req, res) {
      candidatos.create({
@@ -34,4 +35,8 @@ app.post('/add-candidatos', function (req, res) {
     //res.send("Nome: " + req.body.nome + "<br>Cargo: " + req.body.cargo)
 })
 
-app.listen(3000);
+app.listen(3000, function() {
+    console.log("Servidor rodando")
+});
+
+
---
+++ modules/trat_cep.js
@@ -0,0 +1,39 @@
+const limparFormulario = (endereco) => {
+    document.getElementById('logradouro').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('localidade').value = '';
+}
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('logradouro').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('localidade').value = endereco.localidade;
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = ($cep) => $cep.length = 8 && eNumero($cep);
+
+const consultaCep = async() => {
+    limparFormulario();
+
+    const $cep = document.getElementById("cep").value.replace(/\D/g, '');
+    const url = 'https://viacep.com.br/ws/' +  $cep  + '/json/';
+if(cepValido($cep)) {
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    if (endereco.hasOwnProperty('erro')){
+      document.getElementById('endereco').value =  alert ('CEP não encontrado') ;
+    }else{
+        preencherFormulario(endereco);
+    }
+}else{    
+    document.getElementById('endereco').value =  alert ('CEP inválido!');
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout' , consultaCep);    
+    
+}
+
+module.exports = app;
